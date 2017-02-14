80eea923d9c60b3555ac4f3296a1c309c8fb884e
IcyFreakz <wolfyfreakz@hotmail.com>
2017-02-14 17:48:30 +0800

    Level2

---
+++ Assets/Scripts/Level2/WagonMoveForward.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WagonMoveForward : MonoBehaviour {
+    float movementSpeed;
+    float z;
+    // Use this for initialization
+    void Start () {
+        movementSpeed = 0.2f;
+        z = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        z += Time.deltaTime * movementSpeed;
+        gameObject.GetComponent<Transform>().Translate(0, 0, z);
+        if (z > 0.4f)
+        {
+            gameObject.GetComponent<Transform>().localPosition = new Vector3(1.313234f, 0.02144068f, -2.48f);
+            z = 0.0f;
+        }
+    }
+}
