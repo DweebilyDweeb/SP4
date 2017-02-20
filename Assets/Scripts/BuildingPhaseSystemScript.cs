4560a79883a8ae2609155c6166007138f2aefffa
IcyFreakz <wolfyfreakz@hotmail.com>
2017-02-20 10:30:31 +0800

    UI, Sound and Towerbuilding

---
+++ Assets/Scripts/BuildingPhaseSystemScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPhaseSystemScript : MonoBehaviour {
+
+    public SelectedGridScript selectingGrid;
+
+    public int amountToBuildTowers { get; set; }
+    public int numberOfBuildableWalls { get; set;}
+
+	// Use this for initialization
+	void Start () {
+	    if(selectingGrid == null)
+        {
+            Debug.Log("No selecting grid to debug NOOB");
+            return;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
